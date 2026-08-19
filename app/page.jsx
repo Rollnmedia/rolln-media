aa7a0ace89f396c7374b699271ce0ef4510deec0
Rollnmedia <connect.rollnmedia@gmail.com>
2026-08-20 01:18:25 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Gap from "./components/Gap";
 import Engine from "./components/Engine";
 import Work from "./components/Work";
 import About from "./components/About";
+import FinalCTA from "./components/FinalCTA";
 export default function Home() {
   return (
     <main>
@@ -13,6 +14,7 @@ export default function Home() {
       <Engine />
       <Work />
       <About />
+      <FinalCTA />
     </main>
   );
 }
