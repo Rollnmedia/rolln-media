e24d01303f894a981a2da5d8bb6ea0c878502ac8
Rollnmedia <connect.rollnmedia@gmail.com>
2026-08-20 00:56:23 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Gap from "./components/Gap";
+import Engine from "./components/Engine";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Gap />
+      <Engine />
     </main>
   );
 }
