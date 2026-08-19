257668d59919c21f27193a12150a51542f426a6a
Rollnmedia <connect.rollnmedia@gmail.com>
2026-08-20 00:47:02 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,7 @@
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-
 export default function Home() {
   return (
-    <main className="site">
-      <Header />
-      <Hero />
+    <main>
+      <h1>Roll’n Media</h1>
     </main>
   );
 }
