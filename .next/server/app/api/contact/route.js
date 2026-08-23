eab401720d24c54a0cd743beea8e5667f587ff37
Rollnmedia <connect.rollnmedia@gmail.com>
2026-08-23 18:29:03 +0000

    Add contact enquiry form

---
+++ .next/server/app/api/contact/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/contact/route.js")
+R.c("server/chunks/[externals]_next_dist_8dbe5856._.js")
+R.c("server/chunks/[root-of-the-server]__f408c708._.js")
+R.c("server/chunks/node_modules_ca0088eb._.js")
+R.c("server/chunks/_next-internal_server_app_api_contact_route_actions_0bce5875.js")
+R.m(82502)
+module.exports=R.m(82502).exports
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"U7j25THQz5lbsE9lm6kMb3IFhnzq14zzfkjzqDVkLm0=\"\n}"
